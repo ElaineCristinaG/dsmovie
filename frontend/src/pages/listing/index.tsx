b26a74e020eb73dcd0ca6391e3d3d4799fa9ea19
Elaine Cristina <laine.c.g.carminhola@gmail.com>
2022-05-15 19:41:20 -0300

    useState, useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -2,19 +2,25 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCards";
 import axios from "axios";
 import { BASE_URL } from "utils/request";
-
-//forma errada
-
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
-.then(response => {
-    console.log(response.data);
-
-});
-
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing(){
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+           const data = response.data as MoviePage;
+           console.log(data);
+           setPageNumber(data.number);
+    });
+    }, []);
+
     return (
         <> 
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
